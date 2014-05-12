7e1befba8d6fc340b0e60998ae29506ea154896d
Max Lapan <max.lapan@gmail.com>
2014-05-12 13:19:56 +0400

    Resolve colors problem

--- tables/charts.py
+++ tables/charts.py
@@ -42,11 +42,10 @@ def generate_pls (items, title, yaxis, mult=1, suffix="", area=True, color_idx=N
       stubmult: %(mult)s
 """ % {'data': data, 'title': title, 'item_indices': ",".join (map (str, range (2, len (items)+2))),
        'yaxis': yaxis, 'mult': mult}
-
     colors = ['oceanblue', 'yellowgreen', 'orange', 'tan2', 'green', 'darkblue',
-              'purple', 'red', 'coral', 'gray(0.3)', 'teal', 'magenta', 'skyblue',
-              'lightorange', 'tan1', 'lavender', 'pink', 'claret', 'lightpurple', 
-              'redorange', 'black']
+			              'purple', 'red', 'coral', 'gray(0.3)', 'teal', 'magenta', 'skyblue',
+						                'lightorange', 'tan1', 'lavender', 'pink', 'claret', 'lightpurple', 
+										              'redorange', 'black']*5
 
     for i, item in enumerate (reversed (items)):
         if color_idx == None:
@@ -104,7 +103,7 @@ def generate_chart (pls_file):
     # start ploticus
     imgname = os.tmpnam ()
 
-    p = subprocess.Popen (["ploticus", "-font", "FreeSans", "-png", "-o", imgname, pls_file],
+    p = subprocess.Popen (["pl", "-font", "FreeSans", "-png", "-o", imgname, pls_file],
                           env={"GDFONTPATH": os.getcwd ()},
                           stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     stdout, stderr = p.communicate ()
